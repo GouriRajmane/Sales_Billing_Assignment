77c20d88dcfe4f4524f097c92cc4317eaae6dbcf
Gouri Rajmane <rajmanegouri@gmail.com>
2026-08-25 17:44:30 +0530

    Added the Data folder with SalesBillingDBContext.cs and Updated the Web.config with Connection String

---
+++ Sales_Billing_System/Data/SalesBillingDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System.Data.Entity;
+using Sales_Billing_System.Models;
+
+namespace Sales_Billing_System.Data
+{
+    public class SalesBillingDbContext : DbContext
+    {
+        public SalesBillingDbContext()
+            : base("name=SalesBillingDbContext")
+        {
+        }
+
+        public DbSet<Product_Master> Products { get; set; }
+
+        public DbSet<Customer_Master> Customers { get; set; }
+
+        public DbSet<Sales_Invoice> SalesInvoices { get; set; }
+
+        public DbSet<Sales_Invoice_Item> SalesInvoiceItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Customer -> Sales Invoices
+            modelBuilder.Entity<Sales_Invoice>()
+                .HasRequired(i => i.Customer)
+                .WithMany(c => c.SalesInvoices)
+                .HasForeignKey(i => i.CustomerId)
+                .WillCascadeOnDelete(false);
+
+            // Sales Invoice -> Invoice Items
+            modelBuilder.Entity<Sales_Invoice_Item>()
+                .HasRequired(i => i.SalesInvoice)
+                .WithMany(s => s.InvoiceItems)
+                .HasForeignKey(i => i.InvoiceId)
+                .WillCascadeOnDelete(true);
+
+            // Product -> Invoice Items
+            modelBuilder.Entity<Sales_Invoice_Item>()
+                .HasRequired(i => i.Product)
+                .WithMany(p => p.SalesInvoiceItems)
+                .HasForeignKey(i => i.ProductId)
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
