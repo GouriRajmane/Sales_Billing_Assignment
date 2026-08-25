8e5c5c2ff152bf8da60d55ecfb81a52f7f1b55c0
Gouri Rajmane <rajmanegouri@gmail.com>
2026-08-25 17:08:27 +0530

    Created the model classes

---
+++ Sales_Billing_System/Models/Customer_Master.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sales_Billing_System.Models
+{
+    public class Customer_Master
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Customer Name is required.")]
+        [StringLength(100)]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = "Mobile Number is required.")]
+        [StringLength(15)]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Mobile Number should contain only numbers.")]
+        public string MobileNumber { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [StringLength(15)]
+        public string GSTIN { get; set; }
+
+        public virtual ICollection<Sales_Invoice> SalesInvoices { get; set; }
+            = new List<Sales_Invoice>();
+    }
+}
---
+++ Sales_Billing_System/Models/Product_Master.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sales_Billing_System.Models
+{
+    public class Product_Master
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required.")]
+        [StringLength(100)]
+        public string ProductName { get; set; }
+
+        [StringLength(50)]
+        public string SKU { get; set; }
+
+        [Required(ErrorMessage = "Unit is required.")]
+        [StringLength(20)]
+        public string Unit { get; set; }
+
+        [Required(ErrorMessage = "Selling Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Selling Price must be greater than zero.")]
+        public decimal SellingPrice { get; set; }
+
+        [Range(0, 100, ErrorMessage = "GST % must be between 0 and 100.")]
+        public decimal? GSTPercentage { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public virtual ICollection<Sales_Invoice_Item> SalesInvoiceItems { get; set; }
+            = new List<Sales_Invoice_Item>();
+    }
+}
