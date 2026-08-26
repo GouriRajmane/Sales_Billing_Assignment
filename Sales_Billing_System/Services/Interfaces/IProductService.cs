5c4fb97f137e0fe7d5aa8586de47c827aaa21947
Gouri Rajmane <rajmanegouri@gmail.com>
2026-08-26 14:16:06 +0530

    Added the Service Section

---
+++ Sales_Billing_System/Services/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sales_Billing_System.Services.Interfaces
+{
+    public class IProductService
+    {
+    }
+}
