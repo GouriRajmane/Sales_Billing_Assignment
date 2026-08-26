4afcc1c634979fa00a1a6fde69094901797412b9
Gouri Rajmane <rajmanegouri@gmail.com>
2026-08-26 15:06:05 +0530

    Completed the ProductRepositories with implementation of all methods.

--- Sales_Billing_System/Repositories/Interfaces/IProductRepository.cs
+++ Sales_Billing_System/Repositories/Interfaces/IProductRepository.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Sales_Billing_System.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Sales_Billing_System.Repositories.Interfaces
 {
-    public class IProductRepository
+    public interface IProductRepository
     {
+        // Get all products
+        List<Product_Master> GetAllProducts();
+
+        // Get a single product by ID
+        Product_Master GetProductById(int productId);
+
+        // Add a new product
+        void AddProduct(Product_Master product);
+
+        // Update an existing product
+        void UpdateProduct(Product_Master product);
+
+        // Activate or Deactivate product
+        void ToggleStatus(int productId);
+
+        // Search products
+        List<Product_Master> SearchProduct(string searchText);
     }
 }
--- Sales_Billing_System/Repositories/ProductRepository.cs
+++ Sales_Billing_System/Repositories/ProductRepository.cs
@@ -1,11 +1,92 @@
-﻿using System;
+﻿using Sales_Billing_System.Data;
+using Sales_Billing_System.Models;
+using Sales_Billing_System.Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 
 namespace Sales_Billing_System.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : IProductRepository
     {
+        private readonly SalesBillingDbContext _context;
+
+        public ProductRepository(SalesBillingDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get List of all products
+        public List<Product_Master> GetAllProducts()
+        { 
+            return _context.Products
+                            .OrderByDescending(p => p.ProductId)
+                            .ToList();
+        }
+
+        //Get product by ID
+        public Product_Master GetProductById(int productId)
+        {
+            return _context.Products
+                            .FirstOrDefault(p => p.ProductId == productId);
+        }
+
+        // Add a new product
+        public void AddProduct(Product_Master product)
+        { 
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+
+        // Update existing product
+        public void UpdateProduct(Product_Master product)
+        {
+            Product_Master existingProduct = GetProductById(product.ProductId);
+
+            if (existingProduct != null)
+            {
+                existingProduct.ProductName = product.ProductName;
+                existingProduct.SKU = product.SKU;
+                existingProduct.Unit = product.Unit;
+                existingProduct.SellingPrice = product.SellingPrice;
+                existingProduct.GSTPercentage = product.GSTPercentage;
+                existingProduct.IsActive = product.IsActive;
+                existingProduct.UpdatedAt = DateTime.Now;
+
+                _context.SaveChanges();
+            }
+        }
+
+        public void ToggleStatus(int productId)
+        {
+            Product_Master product = GetProductById(productId);
+
+            if (product != null)
+            {
+                product.IsActive = !product.IsActive;
+                product.UpdatedAt = DateTime.Now;
+
+                _context.SaveChanges();
+            }
+        }
+
+        // Search products
+        public List<Product_Master> SearchProduct(string searchText)
+        {
+            if (string.IsNullOrWhiteSpace(searchText))
+            { 
+                return GetAllProducts();
+            }
+
+            return _context.Products
+                            .Where(p =>
+                            p.ProductName.Contains(searchText) || 
+                            (p.SKU!= null && p.SKU.Contains(searchText)))
+                            .OrderByDescending(p => p.ProductId)
+                            .ToList();
+        }
+
+
     }
 }
