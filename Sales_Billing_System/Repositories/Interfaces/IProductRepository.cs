68c2786afadbdff6ea563d0d23fe6428578d9007
Gouri Rajmane <rajmanegouri@gmail.com>
2026-08-26 14:12:50 +0530

    Added the Repositiories Seciton

---
+++ Sales_Billing_System/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sales_Billing_System.Repositories.Interfaces
+{
+    public class IProductRepository
+    {
+    }
+}
