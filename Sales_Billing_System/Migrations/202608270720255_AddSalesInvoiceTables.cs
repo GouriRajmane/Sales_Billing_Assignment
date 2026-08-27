f0419d1e6fed22b25cb211ca78f750df8f9e0dfd
Gouri Rajmane <rajmanegouri@gmail.com>
2026-08-27 13:06:19 +0530

    Updated the Migrations for the SalesInvoice table

---
+++ Sales_Billing_System/Migrations/202608270720255_AddSalesInvoiceTables.cs
@@ -0,0 +1,20 @@
+﻿namespace Sales_Billing_System.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddSalesInvoiceTables : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Sales_Invoice_Item", "GSTPercentage", c => c.Decimal(nullable: false, precision: 5, scale: 2));
+            AlterColumn("dbo.Product_Master", "GSTPercentage", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Product_Master", "GSTPercentage", c => c.Decimal(precision: 18, scale: 2));
+            AlterColumn("dbo.Sales_Invoice_Item", "GSTPercentage", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+        }
+    }
+}
