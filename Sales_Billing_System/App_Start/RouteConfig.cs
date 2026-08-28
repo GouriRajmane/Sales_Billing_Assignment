c3b1dd354e8129ab8320d79d74d7ff8e7f19dec8
Gouri Rajmane <rajmanegouri@gmail.com>
2026-08-28 15:36:26 +0530

    Final Commit.

--- Sales_Billing_System/App_Start/RouteConfig.cs
+++ Sales_Billing_System/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Sales_Billing_System
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "SalesInvoice", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
